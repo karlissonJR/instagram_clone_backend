3bc91e633494e285afd6a753dd95fa09add0f38b
karlissonJR <karlissonj@gmail.com>
2020-05-17 17:02:51 -0300

    user password encryption

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,8 +2,18 @@ const User = require('../models/User');
 
 module.exports = {
     async create(request, response) {
+
+        const { email } = request.body;
+
         try{
+
+            if(await User.findOne({ email }))
+                return response.status(400).send({ error : 'Email Already exists'});
+
             const user = await User.create(request.body);
+
+            user.password = undefined;
+
             return response.json(user);
         }
         catch (error) {
