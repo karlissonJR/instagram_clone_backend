dc82bc6179343659233cd25806e4c394bfbbc9ba
karlissonJR <karlissonj@gmail.com>
2020-05-18 17:16:08 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 require('./database/connection');
 
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
